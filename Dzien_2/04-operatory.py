7be4fc0978a1ede21fad15cc9a4c974e3708701b
Alla <allapilgun@mail.ru>
2018-09-26 19:42:39 +0200

    Dzien 2

---
+++ Dzien_2/04-operatory.py
@@ -0,0 +1,12 @@
+a = 10
+b = 4
+
+print("{} + {} = {}".format(a, b, a + b))
+print("{} - {} = {}".format(a, b, a - b))
+print("{} * {} = {}".format(a, b, a * b))
+print("{} / {} = {}".format(a, b, a / b))  #print("{} / {} = {}".format(a, float(b), a / b))  - w Python2 żeby wynik nie był 2
+print("{} // {} = {}".format(a, b, a // b))
+print("{} % {} = {}".format(a, b, a % b))
+print("{} ** {} = {}".format(a, b, a + b))
+
+print(a, b)
